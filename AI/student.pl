4b0b11956cf5a155d3c15e6f630296d536608ca9
vikash kumar <iit2016041@iiita.ac.in>
2018-08-21 17:21:10 +0530

    Update student.pl

--- AI/student.pl
+++ AI/student.pl
@@ -1,13 +1,35 @@
-roll_no(ram, s001).
-roll_no(shyam, s002).
-roll_no(hanuman, s003).
-roll_no(radha, s004).
-roll_no(sonu, s005).
-roll_no(monu, s006).
-roll_no(tony, s007).
-roll_no(sweety, s008).
-roll_no(john, s009).
+use_module(library(aggregate)).
+
+roll_no(ram, s0001).
+roll_no(shyam, s0002).
+roll_no(hanuman, s0003).
+roll_no(radha, s0004).
+roll_no(sonu, s0005).
+roll_no(monu, s0006).
+roll_no(tony, s0007).
+roll_no(sweety, s0008).
+roll_no(john, s0009).
 roll_no(ballu, s010).
+roll_no(ram1, s0010).
+roll_no(shyam1, s0020).
+roll_no(hanuman1, s0030).
+roll_no(radha1, s0040).
+roll_no(sonu1, s0050).
+roll_no(monu1, s0060).
+roll_no(tony1, s0070).
+roll_no(sweety1, s0080).
+roll_no(john1, s0090).
+roll_no(ballu1, s011).
+roll_no(ram3, s0051).
+roll_no(shyam3, s0022).
+roll_no(hanuman3, s0013).
+roll_no(radha3, s0014).
+roll_no(sonu3, s0015).
+roll_no(monu3, s0016).
+roll_no(tony3, s0017).
+roll_no(sweety3, s0018).
+roll_no(john3, s0019).
+roll_no(ballu3, s0101).
 
 student_type(ram, b_tech).
 student_type(shyam, m_tech).
@@ -19,32 +41,196 @@ student_type(tony, rs).
 student_type(sweety, rs).
 student_type(john, mba).
 student_type(ballu, m_tech).
+student_type(ram1, b_tech).
+student_type(shyam1, m_tech).
+student_type(hanuman1, m_tech).
+student_type(radha1, b_tech).
+student_type(sonu1, b_tech).
+student_type(monu1, mba).
+student_type(tony1, rs).
+student_type(sweety1, rs).
+student_type(john1, mba).
+student_type(ballu1, m_tech).
+student_type(ram3, b_tech).
+student_type(shyam3, m_tech).
+student_type(hanuman3, m_tech).
+student_type(radha3, b_tech).
+student_type(sonu3, b_tech).
+student_type(monu3, mba).
+student_type(tony3, rs).
+student_type(sweety3, rs).
+student_type(john3, mba).
+student_type(ballu3, m_tech).
 
-branch(ram, IT).
-branch(shyam, ECE).
-branch(hanuman, CSE).
-branch(radha, ECE).
-branch(sonu, IT).
-branch(monu, IT).
-branch(tony, ECE).
-branch(sweety, IT).
-branch(john, IT).
-branch(ballu, ECE).
+branch(ram, it).
+branch(shyam, ece).
+branch(hanuman, cse).
+branch(radha, ece).
+branch(sonu, it).
+branch(monu, it).
+branch(tony, ece).
+branch(sweety, it).
+branch(john, it).
+branch(ballu, ece).
+branch(ram, it).
+branch(shyam, ece).
+branch(hanuman, cse).
+branch(radha, ece).
+branch(sonu3, it).
+branch(monu3, it).
+branch(tony3, ece).
+branch(sweety3, it).
+branch(john3, it).
+branch(ballu3, ece).
+branch(ram1, it).
+branch(shyam1, ece).
+branch(hanuman1, cse).
+branch(radha1, ece).
+branch(sonu1, it).
+branch(monu1, it).
+branch(tony1, ece).
+branch(sweety1, it).
+branch(john1, it).
+branch(ballu1, ece).
 
-state(ram, UP).
-state(shyam, MP).
-state(hanuman, HP).
-state(radha, Bihar).
-state(sonu, AP).
-state(monu, Delhi).
-state(tony, Punjab).
-state(sweety, UP).
-state(john, UP).
-state(ballu, Rajsthan).
+state(ram, up).
+state(shyam, mp).
+state(hanuman, hp).
+state(radha, bihar).
+state(sonu, ap).
+state(monu, delhi).
+state(tony, punjab).
+state(sweety, up).
+state(john, bangal).
+state(ballu, rajsthan).
+state(ram1, up).
+state(shyam1, mp).
+state(hanuman1, hp).
+state(radha1, bihar).
+state(sonu1, ap).
+state(monu1, rajsthan).
+state(tony1, punjab).
+state(sweety1, up).
+state(john1, bangal).
+state(ballu1, up).
+state(ram3, j_and_k).
+state(shyam3, mp).
+state(hanuman3, hp).
+state(radha3, bihar).
+state(sonu3, ap).
+state(monu3, rajsthan).
+state(tony3, punjab).
+state(sweety3, kerala).
+state(john3, bangal).
+state(ballu3, up).
+
+intrest(ram,machine_learning).
+intrest(shyam,machine_learning).
+intrest(hanuman,appDev).
+intrest(radha, design).
+intrest(sonu,machine_learning).
+intrest(monu, cyber_security).
+intrest(tony,iot).
+intrest(sweety,machine_learning).
+intrest(john,machine_learning).
+intrest(ballu,software_eng).
+intrest(ram2,machine_learning).
+intrest(shyam2,machine_learning).
+intrest(hanuman2,appDev).
+intrest(radha2, design).
+intrest(sonu2,machine_learning).
+intrest(monu2, cyber_security).
+intrest(tony2,machine_learning).
+intrest(sweety2,iot).
+intrest(john2,software_eng).
+intrest(ballu2,machine_learning).
+intrest(ram3,software_eng).
+intrest(shyam3,machine_learning).
+intrest(hanuman3,appDev).
+intrest(radha3, design).
+intrest(sonu3,machine_learning).
+intrest(monu3, cyber_security).
+intrest(tony3,webDev).
+intrest(sweety3,machine_learning).
+intrest(john3,webDev).
+intrest(ballu3,machine_learning).
+
+mob(ram,9245127361).
+mob(shyam,9245127361).
+mob(hanuman,9445127361).
+mob(radha,9645127361).
+mob(sonu,9245127361).
+mob(monu,9445127361).
+mob(tony,9242347333).
+mob(sweety,9241119361).
+mob(john,9455173616).
+mob(ballu,9945127361).
+mob(ram,9545127361).
+mob(shyam,9845127361).
+mob(hanuman,9945127361).
+mob(radha,9845127361).
+mob(sonu2,9245127388).
+mob(monu2,9445127366).
+mob(tony2,9242347338).
+mob(sweety2,9241119365).
+mob(john2,9455173615).
+mob(ballu2,9945127364).
+mob(ram3,8245127361).
+mob(shyam3,8245127361).
+mob(hanuman3,8445127361).
+mob(radha3,8645127361).
+mob(sonu3,8245127361).
+mob(monu3,8445127361).
+mob(tony3,8242847333).
+mob(sweety3,8241119361).
+mob(john3,8455173616).
+mob(ballu3,8945127361).
 
 
+cgpa(ram,9.2).
+cgpa(shyam,8.2).
+cgpa(hanuman,7.2).
+cgpa(radha,9.9).
+cgpa(sonu,9.6).
+cgpa(monu,9.3).
+cgpa(tony,7.3).
+cgpa(sweety,8.9).
+cgpa(john,6.9).
+cgpa(ballu,7.4).
+cgpa(ram1,9.2).
+cgpa(shyam1,8.2).
+cgpa(hanuman1,7.2).
+cgpa(radha1,9).
+cgpa(sonu1,9.6).
+cgpa(monu1,9.3).
+cgpa(tony1,7.7).
+cgpa(sweety1,8.9).
+cgpa(john1,6.9).
+cgpa(ballu1,7.2).
+cgpa(ram3,9.2).
+cgpa(shyam3,8.2).
+cgpa(hanuman3,7.2).
+cgpa(radha3,7.9).
+cgpa(sonu3,9.6).
+cgpa(monu3,6.3).
+cgpa(tony3,7.3).
+cgpa(sweety3,8.9).
+cgpa(john3,6.9).
+cgpa(ballu3,7.4).
 
+%interest of each branch student ?
+intreest_branch(X,Y) :- intrest(A,X),branch(A,Y) . 
 
-# x=roll_no      y=type
+%what is strength of each branch ?
+strength_of_branch(X) :- aggregate_all(count, branch(_,X), Count),
+						write(Count).
+
+%what is strength from each state ?
+total_student_of_state(X) :- aggregate_all(count, state(_,X), Count),
+						write(Count).
+
+%student who got max cgpa ?
+max_cgpa(X, Y) :- cgpa(X, Y), not( ((cgpa(_,B)), B > Y)).
+
+%write roll of each type ? like b.tech, m.tech etc
 roll_no_of_branch(X,Y) :- student_type(Z,Y),roll_no(Z,X).
-
